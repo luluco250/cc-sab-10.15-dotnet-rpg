3a419aef59a05ce89468de7aa6e578b4262c2c60
Lucas Melo <luluco250@gmail.com>
2019-05-11 13:12:43 -0300

    Made the pawns have color.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,5 +1,36 @@
+using System;
 using UnityEngine;
 
 public class Player : MonoBehaviour {
     public PlayerData data;
+    public BoardData boardData;
+    new Renderer renderer;
+
+    void Awake() {
+        renderer = GetComponent<MeshRenderer>();
+    }
+
+    void Start() {
+        renderer.material.color = GetColorFromType(data.cellType);
+    }
+
+    void Update() {
+        transform.position =
+            boardData.grid.GetCellCenterLocal((Vector3Int)data.position);
+    }
+
+    public static Color GetColorFromType(Board.Cell cellType) {
+        switch (cellType) {
+            case Board.Cell.PlayerBlue:
+                return Color.blue;
+            case Board.Cell.PlayerRed:
+                return Color.red;
+            case Board.Cell.PlayerPurple:
+                return Color.magenta;
+            case Board.Cell.PlayerYellow:
+                return Color.yellow;
+            default:
+                throw new Exception("No color for " + cellType);
+        }
+    }
 }
---
+++ Assets/Scripts/ScriptableObjects/BoardData.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName="Data/BoardData", fileName="BoardData")]
+public class BoardData : ScriptableObject {
+    public Grid grid;
+}
