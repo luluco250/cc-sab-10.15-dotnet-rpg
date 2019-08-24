530107e938a7e8f733e9788d357bd3d8482eb3f3
Lucas Melo <luluco250@gmail.com>
2019-08-24 12:41:22 -0300

    Added option to not move.

--- Assets/Scripts/UI/DirectionButton.cs
+++ Assets/Scripts/UI/DirectionButton.cs
@@ -1,17 +1,24 @@
 ﻿using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DirectionButton : MonoBehaviour {
 	public enum Direction {
 		Up = 0,
 		Right = 1,
 		Down = 2,
-		Left = 3
+		Left = 3,
+		None = 4
 	}
 
 	static readonly float[] DirectionAngles = new float[]{0, 270, 180, 90};
 
 	Direction direction = Direction.Up;
+	Graphic graphic;
+
+	void Awake() {
+		graphic = GetComponent<Graphic>();
+	}
 
 	void Start() {
 		ApplyDirection();
@@ -19,15 +26,20 @@ public class DirectionButton : MonoBehaviour {
 
     public void OnClick() {
 		int dir = (int)direction;
-		dir = (dir + 1) % 4;
+		dir = (dir + 1) % 5;
 		direction = (Direction)dir;
 		ApplyDirection();
 	}
 
 	void ApplyDirection() {
-		var rot = transform.localRotation.eulerAngles;
-		rot.z = DirectionAngles[(int)direction];
-		transform.localRotation = Quaternion.Euler(rot);
+		if (direction == Direction.None) {
+			graphic.color = Color.clear;
+		} else {
+			graphic.color = Color.white;
+			var rot = transform.localRotation.eulerAngles;
+			rot.z = DirectionAngles[(int)direction];
+			transform.localRotation = Quaternion.Euler(rot);
+		}
 	}
 	
 	public Direction GetDirection() => direction;
